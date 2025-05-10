8070cac2f5e83241fdde6b5f2c248fa4039fc74d
Daniel-Maybach <124705897+flini1we@users.noreply.github.com>
2025-05-10 11:09:23 +0300

    Update SecurityConfig.java

--- oris2Sample 2/src/main/java/com/example/demo/config/SecurityConfig.java
+++ oris2Sample 2/src/main/java/com/example/demo/config/SecurityConfig.java
@@ -34,7 +34,5 @@ public class SecurityConfig {
     }
 
     @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
+    public PasswordEncoder passwordEncoder() { return new BCryptPasswordEncoder(); }
 } 
